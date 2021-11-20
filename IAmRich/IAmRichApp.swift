09e7d7ba53e506a8885e34bfdea450c2a454df93
abdel-elsayed <abdel.elsayed18@gmail.com>
2021-11-20 13:14:56 -0500

    Initial Commit

---
+++ IAmRich/IAmRichApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichApp.swift
+//  IAmRich
+//
+//  Created by Abdelrahman Elsayed on 11/20/21.
+//
+
+import SwiftUI
+
+@main
+struct IAmRichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
